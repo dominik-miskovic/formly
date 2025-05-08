84456e360402531c9a720703b89ce714f821b56d
dominikmiskovic <dominikmiskovic291098@gmail.com>
2025-05-09 00:20:27 +0200

    Initial commit

---
+++ src/test/java/com/dominikmiskovic/forumly/ForumlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominikmiskovic.forumly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
