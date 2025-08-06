d7c43f6777b4526702e8fda3dff82f1b8cb673cc
dominikmiskovic <dominikmiskovic291098@gmail.com>
2025-08-06 23:15:49 +0200

    Restrict homepage access to authenticated users in WebSecurityConfig

--- src/main/java/com/dominikmiskovic/forumly/config/WebSecurityConfig.java
+++ src/main/java/com/dominikmiskovic/forumly/config/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/register").permitAll()
+                        .requestMatchers("/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
